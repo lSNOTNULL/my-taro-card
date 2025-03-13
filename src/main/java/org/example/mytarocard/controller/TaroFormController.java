e9ca8f32b8eefe55705dcceb5ceb75ab0eabeccd
qus0in <qus0in@gmail.com>
2025-03-13 17:22:54 +0900

    supabase

--- src/main/java/org/example/mytarocard/controller/TaroFormController.java
+++ src/main/java/org/example/mytarocard/controller/TaroFormController.java
@@ -9,6 +9,8 @@ import org.example.mytarocard.model.dto.LLMServiceParam;
 import org.example.mytarocard.model.dto.LLMServiceResponse;
 import org.example.mytarocard.service.LLMService;
 import org.example.mytarocard.service.LLMServiceImpl;
+import org.example.mytarocard.service.SupabaseService;
+import org.example.mytarocard.service.SupabaseServiceImpl;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -16,11 +18,13 @@ import java.util.UUID;
 @WebServlet("/")
 public class TaroFormController extends Controller {
     private LLMService llmService;
+    private SupabaseService supabaseService;
 
     @Override
     public void init() throws ServletException {
         log("TaroFormController Init");
          llmService = LLMServiceImpl.getInstance();
+         supabaseService = SupabaseServiceImpl.getInstance();
     }
 
     @Override
@@ -50,6 +54,11 @@ public class TaroFormController extends Controller {
         // 겹칠 확률이 극도로 낮은 임의의 문자열 값
         String uuid = UUID.randomUUID().toString();
         // 새로운 호출로 처리하겠네...
+        try {
+            supabaseService.save(uuid, response.data(), "");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         resp.sendRedirect(req.getContextPath() + "/result/%s".formatted(uuid));
     }
---
+++ src/main/java/org/example/mytarocard/service/SupabaseServiceImpl.java
@@ -0,0 +1,19 @@
+package org.example.mytarocard.service;
+
+import org.example.mytarocard.model.repository.SupabaseRepository;
+
+import java.io.IOException;
+
+public class SupabaseServiceImpl implements SupabaseService {
+    private final SupabaseRepository supabaseRepository = SupabaseRepository.getInstance();
+    private SupabaseServiceImpl() {}
+    private static SupabaseServiceImpl instance = new SupabaseServiceImpl();
+    public static SupabaseServiceImpl getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void save(String id, String text, String image) throws IOException, InterruptedException {
+        supabaseRepository.save(id, text, image);
+    }
+}
