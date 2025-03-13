c5bd094d37a94950a03f598f1365cc9a1777d366
qus0in <qus0in@gmail.com>
2025-03-13 12:50:52 +0900

    SEO란 이런 것이다?

--- src/main/java/org/example/mytarocard/controller/TaroFormController.java
+++ src/main/java/org/example/mytarocard/controller/TaroFormController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet("/")
 public class TaroFormController extends Controller {
@@ -17,6 +18,22 @@ public class TaroFormController extends Controller {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log("TaroFormController doGet");
+        req.setAttribute("pageTitle", "타로 좋아! 정말 좋아!");
+        req.setAttribute("ogTitle", "어렸을 때부터 타로를 좋아한 사람이 있었다?");
+        req.setAttribute("ogDescription", "타로타로 타로 밀크티!!!");
+        req.setAttribute("ogImageUrl", "%s/assets/%s".formatted(req.getContextPath(), "taro.jpeg"));
+        req.setAttribute("ogPageUrl", req.getContextPath());
         view(req, resp, "form");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 무언가 처리할 예정!
+
+        // 겹칠 확률이 극도로 낮은 임의의 문자열 값
+        String uuid = UUID.randomUUID().toString();
+        // 새로운 호출로 처리하겠네...
+
+        resp.sendRedirect(req.getContextPath() + "/result/%s".formatted(uuid));
     }
 }
